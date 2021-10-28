f386263fe75f1330d1d6c5ad39c811aa8cedac4a
sherondavesanol <sdssanol@gmail.com>
2021-10-28 20:53:08 +0800

    api

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+let result = "";
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    return res.send("Hello, World");
+});
+
+app.post('/', (req, res) => {
+    result = req.body.result;
+
+    return res.send(result);
+});
+
+app.get('/result', (req, res) => {
+    return res.send(result);
+});
